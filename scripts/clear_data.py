ffdb1b9bee6b06c858d8b23815657a7fb8dbb931
shajen <borysdyczko@gmail.com>
2025-11-05 19:50:15 +0100

    Updated clear_data.

--- scripts/clear_data.py
+++ scripts/clear_data.py
@@ -2,8 +2,10 @@ from sdr.models import *
 
 
 def run(*args):
+    print("Devices: %d" % (Device.objects.count()))
     print("Transmissions: %d" % (Transmission.objects.count()))
     print("Spectrograms: %d" % (Spectrogram.objects.count()))
 
+    Device.objects.all().delete()
     Transmission.objects.all().delete()
     Spectrogram.objects.all().delete()
